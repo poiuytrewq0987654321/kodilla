5b77ebfbaed779ad4fd08dfe8f42a4e512f0ceed
Bartosz Adamiec <bartosz.adamiec@aviva.pl>
2018-09-24 21:35:26 +0200

    commit
    w pełni działające zadanie 4.6 wyszukiwarka krajów

---
+++ kodilla-zad-4.6/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+    countriesList.innerHTML = '';
+    resp.forEach(function(item){
+    var liEl = document.createElement('li');
+    var olEl = document.createElement('ol');
+    liEl.innerText = item.name;
+    olEl.innerText = item.capital;
+    countriesList.appendChild(liEl);
+    countriesList.appendChild(olEl);
+});
+}
+
