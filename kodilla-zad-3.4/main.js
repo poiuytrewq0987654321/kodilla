0a16007828e05cfbd70ca23010f2999d8fb6a024
Bartosz Adamiec <bartosz.adamiec@aviva.pl>
2018-09-25 12:30:15 +0200

    update

--- kodilla-zad-3.4/main.js
+++ kodilla-zad-3.4/main.js
@@ -5,12 +5,12 @@ function Phone(brand, price, color, toDoList) {
     this.toDoList = toDoList;
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". " + this.toDoList);
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var samsungS6 = new Phone("Samsung", 2300, "black or white");
-var onePlus = new Phone("One Plus", 1200, "gold");
+var iPhone6S = new Phone("Apple", 2250, "silver", "kupić w grudniu");
+var samsungS6 = new Phone("Samsung", 2300, "black or white", "za słaby procesor");
+var onePlus = new Phone("One Plus", 1200, "gold", "tani i dobry ale chiński");
 
 iPhone6S.printInfo();
 samsungS6.printInfo();
