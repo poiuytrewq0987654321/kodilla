50db32ed32c9fb5e1674ad6133e45259830a493b
docker <docker@docker.pl>
2019-06-20 21:38:09 +0200

    delete zad-2.2 (mustache vulnerability)

--- kodilla-zad-2.2/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function loadUser() {
-  var template = $('#template').html();
-  Mustache.parse(template);   // optional, speeds up future uses
-  var rendered = Mustache.render(template, {name: "Luke"});
-  $('#target').html(rendered);
-}
